7ac847efb6869ffc944fc8d61bdb26f8ad29e5c6
echizentm <echizentm@gmail.com>
2012-01-30 14:00:26 +0000

    implement testcase of wavelet_tree

--- src/shellinford_wavelet_tree.h
+++ src/shellinford_wavelet_tree.h
@@ -6,19 +6,19 @@
 namespace shellinford {
   class wavelet_tree {
   public:
-    wavelet_tree() {}
-    ~wavelet_tree() {}
-    void clear() {}
-    char get(uint64_t i) const { return 0; }
-    void build(const char *s) {}
+    wavelet_tree();
+    ~wavelet_tree();
+    void clear();
+    char get(uint64_t i) const;
+    void build(const char *s);
     uint64_t size() const { return 0; }
     uint64_t size(char c) const { return 0; }
-    uint64_t rank(uint64_t i, char c) const { return 0; }
-    uint64_t select(uint64_t i, char c) const { return 0; }
-    void write(std::ofstream &ofs) const {}
-    void write(const char *filename) const {} 
-    void read(std::ifstream &ifs) {}
-    void read(const char *filename) {}
+    uint64_t rank(uint64_t i, char c) const;
+    uint64_t select(uint64_t i, char c) const;
+    void write(std::ofstream &ofs) const;
+    void write(const char *filename) const;
+    void read(std::ifstream &ifs);
+    void read(const char *filename);
   };
 }
 
